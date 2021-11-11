d08cc906d41d84bb2561b175c11596e2b2368362
mmiah00 <mmiah00@stuy.edu>
2021-11-10 19:13:03 -0500

    initial Navigation Bar with 4 tabs: Home, Our Story, Starting Up Your Robot, and FAQs

---
+++ NavBar.js
@@ -0,0 +1,12 @@
+function NavBar () { 
+    return (
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#ourstory">Our Story</a>
+            <a href="#setup">Starting Up Your Robot</a>
+            <a href="#faq">FAQs</a>
+        </div>
+    );
+}
+
+export default NavBar; 
