5e3c397b22e0b713fa682c30dbca93a01d0b88dc
mmiah00 <maishm357@gmail.com>
2021-11-29 21:46:32 -0500

    made componentcalled GetInTouch that represents the text at the bottom of every page with out contact information

---
+++ quadruped-website/src/GetInTouch.js
@@ -0,0 +1,15 @@
+function GetInTouch () { 
+    return (
+        <div> 
+            <h3> Get in touch </h3> 
+            <h1> info@Sparky.net</h1>
+            
+            <h3> Follow Us </h3> 
+
+            <h3> Instagram </h3> 
+            <h3> LinkedIn </h3>  
+        </div>  
+    ); 
+}
+
+export default GetInTouch; 
