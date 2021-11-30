32173a51fffa4ea7d032da7d1eeea3f25dd22201
mmiah00 <maishm357@gmail.com>
2021-11-29 22:22:58 -0500

    didn't make much changes to App.js, used it to test AboutUs

--- quadruped-website/src/App.js
+++ quadruped-website/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AboutUs from './AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <AboutUs /> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
