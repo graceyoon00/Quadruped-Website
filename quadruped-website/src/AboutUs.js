5f07db69412fdd10950616b41bb8951c8e92508a
mmiah00 <maishm357@gmail.com>
2021-11-29 20:45:42 -0500

    writing outline of About Us page

---
+++ quadruped-website/src/AboutUs.js
@@ -0,0 +1,13 @@
+function AboutUs () { 
+    return (
+        <div> 
+            <h1> Who We Are <h1/> 
+            <h2> We're Sparkers! </h2> 
+            <h3> a team like no others. <h3> 
+            <p>
+                <b> Our Story </b> 
+                Team Sparky is built upon a unique breadth of students leading \n computational and data driven projects at Boston University. \n Being a startup in Boston, including members from all over the \n world, our team philosophy draws from western development \n principles couples with diverse flair and creativity. 
+            </p>
+        </div> 
+    ); 
+}
